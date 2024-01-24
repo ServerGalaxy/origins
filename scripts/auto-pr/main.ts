3990519d38b1254430cdd91f9815485bd8373ca0
PinedaVictor <pinedavictor095@gmail.com>
2024-01-23 16:09:26 -0800

    fix(blue-49): updated auto-pr to push currecnt branch to remote branch automatically

--- scripts/auto-pr/main.ts
+++ scripts/auto-pr/main.ts
@@ -1,7 +1,20 @@
 import 'dotenv/config';
+import {execSync} from 'child_process';
 import {submitPR, getCurrentBranch} from './github';
 
+// Function to execute shell commands
+function executeCommand(command: string): void {
+  try {
+    execSync(command, {stdio: 'inherit'});
+  } catch (error) {
+    console.error(`Error executing command: ${command}`);
+    // eslint-disable-next-line no-process-exit
+    process.exit(1);
+  }
+}
+
 function main() {
+  executeCommand(`git push origin ${getCurrentBranch()}`);
   submitPR('beta', getCurrentBranch(), getCurrentBranch());
   submitPR('alpha', 'Release Alpha', 'beta');
   submitPR('main', 'Release Production', 'alpha');
