21bc6c8d3259f0c6f47a0c2b168a4d28d9f3dc96
PinedaVictor <pinedavictor095@gmail.com>
2024-01-23 20:51:06 -0800

    feat(blue-49): remoeved route that was no longer needed
    Route no longer needed to be supported due to infra changes and support

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,6 @@ app.get('/', (req: Request, res: Response) => {
   return res.status(200).json({sup: true});
 });
 
-app.get('/woah', (req: Request, res: Response) => {
-  return res.status(200).json({sup: true, endpoint: 'woah'});
-});
-
 app.listen(port, () => {
   // TODO: Update 8080 to port const
   console.log('API listening on port http://localhost:8080/');
