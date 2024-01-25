44c2e2e37eca462dbbe8ec427b7b01343abb4a05
Victor Pineda <pinedavictor095@gmail.com>
2024-01-25 10:32:39 -0800

    BLUE-543 (#43) (#44)
    * feat(blue-543): added new ./serviceC API route to GET user data
    
    * feat: added API route ./serviceD to GET pet data

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,13 @@ app.get('/serviceB', (req: Request, res: Response) => {
   return res.status(200).json({sup: true});
 });
 
+app.get('/serviceC', (req: Request, res: Response) => {
+  return res.status(200).json({sup: true});
+});
+app.get('/serviceD', (req: Request, res: Response) => {
+  return res.status(200).json({sup: true});
+});
+
 app.listen(port, () => {
   // TODO: Update 8080 to port const
   console.log('API listening on port http://localhost:8080/');
