26e144afbd737f5c29f57d5e695ac7277ab8f70d
PinedaVictor <pinedavictor095@gmail.com>
2024-08-06 20:47:46 -0700

    Removed two endpoints and added TODO comment

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,7 @@ app.get('/', (req: Request, res: Response) => {
   return res.status(200).json({sup: true});
 });
 
-app.get('/callback', (req: Request, res: Response) => {
-  return res.status(200).json({sup: true});
-});
-
-app.get('/serviceB', (req: Request, res: Response) => {
-  return res.status(200).json({sup: true});
-});
-
-app.get('/service3', (req: Request, res: Response) => {
-  return res.status(200).json({sup: true});
-});
-
+// TODO: Update server listener
 app.listen(port, () => {
   // TODO: Update 8080 to port const
   console.log('API listening on port http://localhost:8080/');
