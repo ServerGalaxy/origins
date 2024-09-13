fb0c921873e364ca67627c86547d2a2648ca7dc1
PinedaVictor <pinedavictor095@gmail.com>
2024-09-13 10:25:12 -0700

    Added user route to app.ts that returns user name and id

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ app.get('/', (req: Request, res: Response) => {
   return res.status(200).json({sup: true});
 });
 
+app.get('/user', (req: Request, res: Response) => {
+  return res.status(200).json({name: 'Victor', id: '12345'});
+});
+
 app.listen(port, () => {
   console.log('API listening on port http://localhost:8080/');
 });
