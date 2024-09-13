9585c7387e420dadab76030a705ed71efbfa076a
PinedaVictor <pinedavictor095@gmail.com>
2024-09-13 10:30:24 -0700

    Addeed authorization middleware function to get user data. TESTING INSTRUCTIONS: 1. Pull branch 2. Hit user route with undefined  authorization header 3. Hit user route with a defined authorization header

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import type {Request, Response} from 'express';
+import type {NextFunction, Request, Response} from 'express';
 import express from 'express';
 
 const app = express();
@@ -6,6 +6,13 @@ const port = 8080;
 
 app.get('/', (req: Request, res: Response) => {
   return res.status(200).json({sup: true});
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (req.headers.authorization === undefined) {
+    return res.status(403).json({error: 'User Not Authorized'});
+  }
+  next();
 });
 
 app.get('/user', (req: Request, res: Response) => {
